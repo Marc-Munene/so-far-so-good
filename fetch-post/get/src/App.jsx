11ed520f5209bac7cf3d2fadb412c4c2ef1939ec
Marc Munene <marcmunene104@gmail.com>
2025-03-20 14:42:48 +0300

    users data

--- fetch-post/get/src/App.jsx
+++ fetch-post/get/src/App.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const responce = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const json = await responce.json();
+
+        setData(json);
+        console.log(json);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      {data.map((users) => {
+        return (
+          <div>
+            <p>ID:</p>
+            <span>{users.id} </span>
+            <p>Name:</p>
+            <span>{users.name} </span>
+            <p>Username:</p>
+            <span>{users.username}</span>
+            <p>Email:</p>
+            <span>{users.email} </span>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export { App };
